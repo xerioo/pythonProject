47a9c28b65fc8cebf7ea73d15c692e65690b8cbd
xeri <xerioo@gmail.com>
2023-11-11 13:47:04 +0100

    styling

---
+++ kutya.py
@@ -0,0 +1,20 @@
+class Kutya:
+    def __init__(self, nev, kor, faj):
+        self.nev = nev
+        self.kor = kor
+        self.faj = faj
+
+    def emberi_evekben(self):
+        emberi_kor = self.kor * 7  # Alapért.: 1 kutyaév = 7 emberi év
+
+        if self.faj.lower() == 'tacskó':
+            emberi_kor = self.kor * 8  # Ha tacskó: 1 kutyaév = 8 emberi év
+
+        print(f"Ez a kutya: {self.nev}, {self.faj} fajta és {self.kor} kutyaéves, ami emberi években {emberi_kor} év.")
+
+# Tesztelés
+kutya1 = Kutya("Bodri", 3, "Golden Retriever")
+kutya1.emberi_evekben()
+
+kutya2 = Kutya("Fido", 5, "Tacskó")
+kutya2.emberi_evekben()
